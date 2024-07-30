dbb6cbfb4010a14b4df2ccd305bd2695a9774ea7
Emmanuel Sosa <enmnuelsosa63@gmail.com>
2024-07-30 16:34:53 -0400

    Creacion de la carpeta backend de la aplicación

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express'),
+app = express(),
+cors = require('cors');
+
+app.use(cors())
+app.use(express.static('dist'))
+
+const notes = [
+    {
+        "id": "1",
+        "content": "HTML is easy",
+        "important": true
+    },
+    {
+        "id": "2",
+        "content": "Browser can execute only JavaScript",
+        "important": true
+    },
+    {
+        "id": "3",
+        "content": "GET and POST are the most important methods of HTTP protocol",
+        "important": true
+    },
+    {
+        "id": "4",
+        "content": "a new note...",
+        "important": false
+    },
+    {
+        "id": "5",
+        "content": "Learn C#",
+        "important": true
+    },
+    {
+        "id": 6,
+        "content": "Learn PHP",
+        "important": false
+    },
+    {
+        "id": 7,
+        "content": "Learn Swift",
+        "important": false
+    }
+]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+})
+
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
