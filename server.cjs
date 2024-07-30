1b3c3fa9ced6b3a32d211c5582fabbb852729e38
Emmanuel Sosa <enmnuelsosa63@gmail.com>
2024-07-30 09:54:59 -0400

    Actualizando variables

--- server.cjs
+++ server.cjs
@@ -52,7 +52,7 @@ app.get('/api/notes', (request, response) => {
 })
 
 
-let PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 });
